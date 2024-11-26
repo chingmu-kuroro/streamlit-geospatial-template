b7be940ba8876a30baab5460da6be83952d3bc1b
chingmu-kuroro <76989123+chingmu-kuroro@users.noreply.github.com>
2024-11-26 15:45:58 +0800

    Update 6_🚖_NYC_Uber_Ridesharing_Data.py

--- pages/6_🚖_NYC_Uber_Ridesharing_Data.py
+++ pages/6_🚖_NYC_Uber_Ridesharing_Data.py
@@ -8,10 +8,6 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
-# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-# st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
-
-
 # LOAD DATA ONCE
 @st.cache_resource
 def load_data():
